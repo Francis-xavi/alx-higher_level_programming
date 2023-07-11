2808e684c967a607185c86f30a10e3ce7e795423
udo-ndupu bryan <ahunna45@gmail.com>
2023-07-11 02:33:04 -0700

    initial changes

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- displays the number of records with id=89 inthe table first_table of the database hbtn_0c_0 in your MySQL server.
+-- the database name will be passed as an arguement of the mySQL command
+
+SELECT COUNT(*)FROM first_table WHERE id=89;

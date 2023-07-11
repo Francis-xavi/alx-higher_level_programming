288486fa4ee024d43aace7ad872bf2a79811b100
udo-ndupu bryan <ahunna45@gmail.com>
2023-07-11 00:20:47 -0700

    task 3

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- delete databases
+
 DROP DATABASES IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- list all the tables in your MySQL server.
+-- the database will be passed as an arguement of mySQL command
+
+SHOW TABLES;
